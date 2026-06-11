92623214f8c2e130722be58cb338b4e8f2686f5f
Balachandra <balachandralucky2@gmail.com>
2026-06-11 20:46:25 +0530

    ContainerWithMostWater_TwoPointer_Approach - commit

---
+++ src/com/bala/dsa/blind75/twoPointers/containerWithMostWater/ContainerWithMostWater_TwoPointer_Approach.java
@@ -0,0 +1,26 @@
+package com.bala.dsa.blind75.twoPointers.containerWithMostWater;
+
+public class ContainerWithMostWater_TwoPointer_Approach {
+    public static void main(String[] args) {
+        ContainerWithMostWater_TwoPointer_Approach containerWithMostWater_twoPointer_approach = new ContainerWithMostWater_TwoPointer_Approach();
+        int[] nums = {1,7,2,5,4,7,3,6};
+        int result = containerWithMostWater_twoPointer_approach.maxArea(nums);
+        System.out.println("Result: " + result);
+    }
+    public int maxArea(int[] heights) {
+        int l = 0;
+        int r = heights.length - 1;
+        int res = 0;
+
+        while (l < r) {
+            int area = Math.min(heights[l], heights[r]) * (r - l);
+            res = Math.max(res, area);
+            if (heights[l] <= heights[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return res;
+    }
+}
